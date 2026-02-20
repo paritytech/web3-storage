7809ab971be201a2b6ae9c827723a3cdee01ab6c
Naren Mudigal <naren@parity.io>
2026-02-20 22:11:35 +0100

    fix: correct package versions for react-rxjs and lucide-react

--- user-interface-layer/provider-dashboard/src/main.tsx
+++ user-interface-layer/provider-dashboard/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Subscribe } from 'react-rxjs'
+import { Subscribe } from '@react-rxjs/core'
 import App from './App'
 import './index.css'
 
