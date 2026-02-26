9ea5a04c024b9fc04b37b65f1bc9c87de7dc60e4
Naren Mudigal <naren@parity.io>
2026-02-26 13:23:48 +0100

    feat: add S3-compatible storage interface (Layer 1)
    Implement S3-compatible storage interface that provides familiar S3 API
    semantics on top of Layer 0 blob storage. The S3 pallet internally creates
    Layer 0 buckets, encapsulating the storage layer from clients.
    
    Components:
    - s3-primitives: Core types (ObjectMetadata, S3BucketInfo) and validation
    - pallet-s3-registry: On-chain S3 bucket/object metadata with automatic
      Layer 0 bucket creation via create_bucket_internal()
    - s3-client: High-level SDK using subxt 0.44 for chain operations and
      storage-client for provider blob operations
    
    Key features:
    - Bucket operations: create, delete, head, list
    - Object operations: put, get, delete, copy, list with prefix/delimiter
    - S3 naming rules validation (3-63 chars, lowercase alphanumeric + hyphens)
    - Content-addressed storage via CID (blake2-256)
    - User-defined metadata support
    
    The architecture ensures clients only interact with the S3 layer while
    Layer 0 provides the underlying storage guarantees (checkpoints, challenges,
    slashing).

---
+++ storage-interfaces/s3/pallet-s3-registry/src/mock.rs
@@ -0,0 +1,136 @@
+//! Mock runtime for S3 Registry pallet tests.
+
+use crate as pallet_s3_registry;
+use frame_support::{
+	derive_impl, parameter_types,
+	traits::{ConstU32, ConstU64, ConstU128},
+};
+use sp_core::H256;
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage,
+};
+
+type Block = frame_system::mocking::MockBlock<Test>;
+type Balance = u128;
+
+frame_support::construct_runtime!(
+	pub enum Test {
+		System: frame_system,
+		Balances: pallet_balances,
+		StorageProvider: pallet_storage_provider,
+		S3Registry: pallet_s3_registry,
+	}
+);
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Nonce = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Block = Block;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = ConstU32<16>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: Balance = 1;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = ();
+	type MaxReserves = ConstU32<2>;
+	type ReserveIdentifier = [u8; 8];
+	type Balance = Balance;
+	type RuntimeEvent = RuntimeEvent;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type FreezeIdentifier = ();
+	type MaxFreezes = ();
+	type RuntimeHoldReason = ();
+	type RuntimeFreezeReason = ();
+	type DoneSlashHandler = ();
+}
+
+parameter_types! {
+	pub const MinProviderStake: Balance = 1_000_000_000_000;
+	pub const MinStakePerByte: Balance = 1_000;
+	pub const MaxMultiaddrLength: u32 = 128;
+	pub const MaxMembers: u32 = 100;
+	pub const MaxPrimaryProviders: u32 = 5;
+	pub const MaxChunkSize: u32 = 262144;
+	pub const ChallengeTimeout: u64 = 100;
+	pub const SettlementTimeout: u64 = 50;
+	pub const RequestTimeout: u64 = 25;
+	pub const DefaultCheckpointInterval: u64 = 100;
+	pub const DefaultCheckpointGrace: u64 = 20;
+	pub const CheckpointReward: Balance = 1_000_000_000_000;
+	pub const CheckpointMissPenalty: Balance = 500_000_000_000;
+	pub TreasuryAccount: u64 = 999;
+}
+
+impl pallet_storage_provider::Config for Test {
+	type Currency = Balances;
+	type Treasury = TreasuryAccount;
+	type MinStakePerByte = MinStakePerByte;
+	type MaxMultiaddrLength = ConstU32<128>;
+	type MaxMembers = ConstU32<100>;
+	type MaxPrimaryProviders = ConstU32<5>;
+	type MinProviderStake = MinProviderStake;
+	type MaxChunkSize = ConstU32<262144>;
+	type ChallengeTimeout = ChallengeTimeout;
+	type SettlementTimeout = SettlementTimeout;
+	type RequestTimeout = RequestTimeout;
+	type DefaultCheckpointInterval = DefaultCheckpointInterval;
+	type DefaultCheckpointGrace = DefaultCheckpointGrace;
+	type CheckpointReward = CheckpointReward;
+	type CheckpointMissPenalty = CheckpointMissPenalty;
+	type WeightInfo = ();
+}
+
+impl pallet_s3_registry::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type MaxBucketsPerUser = ConstU32<100>;
+	type MaxObjectsPerBucket = ConstU32<10000>;
+}
+
+/// Build test externalities.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let mut t = frame_system::GenesisConfig::<Test>::default().build_storage().unwrap();
+
+	pallet_balances::GenesisConfig::<Test> {
+		balances: vec![
+			(1, 100_000_000_000_000),
+			(2, 100_000_000_000_000),
+			(3, 100_000_000_000_000),
+		],
+		dev_accounts: None,
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| {
+		System::set_block_number(1);
+	});
+	ext
+}
---
+++ storage-interfaces/s3/pallet-s3-registry/src/tests.rs
@@ -0,0 +1,249 @@
+//! Tests for S3 Registry pallet.
+
+use crate::{mock::*, Error, Objects, S3Buckets};
+use frame_support::{assert_noop, assert_ok, BoundedVec};
+use s3_primitives::MaxObjectKeyLen;
+use sp_core::H256;
+
+/// Helper to register a provider.
+fn register_provider(who: u64) {
+	let multiaddr: BoundedVec<u8, frame_support::traits::ConstU32<128>> =
+		b"/ip4/127.0.0.1/tcp/3000".to_vec().try_into().unwrap();
+	let public_key: BoundedVec<u8, frame_support::traits::ConstU32<64>> =
+		b"01234567890123456789012345678901".to_vec().try_into().unwrap();
+	assert_ok!(StorageProvider::register_provider(
+		RuntimeOrigin::signed(who),
+		multiaddr,
+		public_key,
+		1_000_000_000_000, // Stake
+	));
+	assert_ok!(StorageProvider::update_provider_settings(
+		RuntimeOrigin::signed(who),
+		pallet_storage_provider::ProviderSettings {
+			accepting_primary: true,
+			accepting_extensions: true,
+			min_duration: 10,
+			max_duration: 1000,
+			price_per_byte: 1,
+			replica_sync_price: None, // Not accepting replicas
+			max_capacity: 1_000_000_000,
+		}
+	));
+}
+
+#[test]
+fn create_s3_bucket_works() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1, // min_providers
+		));
+
+		let bucket = S3Buckets::<Test>::get(0).unwrap();
+		assert_eq!(bucket.name.as_slice(), b"my-bucket");
+		assert_eq!(bucket.owner, 1);
+		assert_eq!(bucket.object_count, 0);
+		// Layer 0 bucket should have been created automatically
+		assert!(pallet_storage_provider::Buckets::<Test>::get(bucket.layer0_bucket_id).is_some());
+	});
+}
+
+#[test]
+fn create_s3_bucket_fails_invalid_name() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_noop!(
+			S3Registry::create_s3_bucket(RuntimeOrigin::signed(1), b"ab".to_vec(), 1),
+			Error::<Test>::InvalidBucketName
+		);
+
+		assert_noop!(
+			S3Registry::create_s3_bucket(RuntimeOrigin::signed(1), b"MyBucket".to_vec(), 1),
+			Error::<Test>::InvalidBucketName
+		);
+	});
+}
+
+#[test]
+fn create_s3_bucket_fails_duplicate_name() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1,
+		));
+
+		assert_noop!(
+			S3Registry::create_s3_bucket(RuntimeOrigin::signed(1), b"my-bucket".to_vec(), 1),
+			Error::<Test>::BucketNameExists
+		);
+	});
+}
+
+#[test]
+fn delete_s3_bucket_works() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1,
+		));
+
+		assert_ok!(S3Registry::delete_s3_bucket(RuntimeOrigin::signed(1), 0));
+
+		assert!(S3Buckets::<Test>::get(0).is_none());
+	});
+}
+
+#[test]
+fn delete_s3_bucket_fails_not_empty() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1,
+		));
+
+		let cid = H256::repeat_byte(0x42);
+		assert_ok!(S3Registry::put_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"test.txt".to_vec(),
+			cid,
+			100,
+			b"text/plain".to_vec(),
+			vec![],
+		));
+
+		assert_noop!(
+			S3Registry::delete_s3_bucket(RuntimeOrigin::signed(1), 0),
+			Error::<Test>::BucketNotEmpty
+		);
+	});
+}
+
+#[test]
+fn put_object_metadata_works() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1,
+		));
+
+		let cid = H256::repeat_byte(0x42);
+		assert_ok!(S3Registry::put_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"folder/test.txt".to_vec(),
+			cid,
+			1234,
+			b"text/plain".to_vec(),
+			vec![(b"x-custom".to_vec(), b"value".to_vec())],
+		));
+
+		let key: BoundedVec<u8, MaxObjectKeyLen> =
+			b"folder/test.txt".to_vec().try_into().unwrap();
+		let metadata = Objects::<Test>::get(0, &key).unwrap();
+		assert_eq!(metadata.cid, cid);
+		assert_eq!(metadata.size, 1234);
+
+		let bucket = S3Buckets::<Test>::get(0).unwrap();
+		assert_eq!(bucket.object_count, 1);
+		assert_eq!(bucket.total_size, 1234);
+	});
+}
+
+#[test]
+fn delete_object_metadata_works() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"my-bucket".to_vec(),
+			1,
+		));
+
+		let cid = H256::repeat_byte(0x42);
+		assert_ok!(S3Registry::put_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"test.txt".to_vec(),
+			cid,
+			100,
+			b"text/plain".to_vec(),
+			vec![],
+		));
+
+		assert_ok!(S3Registry::delete_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"test.txt".to_vec(),
+		));
+
+		let key: BoundedVec<u8, MaxObjectKeyLen> =
+			b"test.txt".to_vec().try_into().unwrap();
+		assert!(Objects::<Test>::get(0, &key).is_none());
+
+		let bucket = S3Buckets::<Test>::get(0).unwrap();
+		assert_eq!(bucket.object_count, 0);
+		assert_eq!(bucket.total_size, 0);
+	});
+}
+
+#[test]
+fn copy_object_metadata_works() {
+	new_test_ext().execute_with(|| {
+		register_provider(1);
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"bucket1".to_vec(),
+			1,
+		));
+
+		assert_ok!(S3Registry::create_s3_bucket(
+			RuntimeOrigin::signed(1),
+			b"bucket2".to_vec(),
+			1,
+		));
+
+		let cid = H256::repeat_byte(0x42);
+		assert_ok!(S3Registry::put_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"source.txt".to_vec(),
+			cid,
+			100,
+			b"text/plain".to_vec(),
+			vec![],
+		));
+
+		assert_ok!(S3Registry::copy_object_metadata(
+			RuntimeOrigin::signed(1),
+			0,
+			b"source.txt".to_vec(),
+			1,
+			b"copy.txt".to_vec(),
+		));
+
+		let key: BoundedVec<u8, MaxObjectKeyLen> =
+			b"copy.txt".to_vec().try_into().unwrap();
+		let metadata = Objects::<Test>::get(1, &key).unwrap();
+		assert_eq!(metadata.cid, cid);
+		assert_eq!(metadata.size, 100);
+	});
+}
