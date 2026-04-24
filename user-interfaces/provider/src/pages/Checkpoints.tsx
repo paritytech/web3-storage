b4e9ce45bfdae8e8f492f00e00c30b705f001057
Ilia Churin <ilia@parity.io>
2026-04-24 14:09:21 +0900

    fix: display block number instead of unix time on provider snapshot page

--- user-interfaces/provider/src/pages/Checkpoints.tsx
+++ user-interfaces/provider/src/pages/Checkpoints.tsx
@@ -67,7 +67,7 @@ function CheckpointsContent() {
           <CardContent>
             <div className="text-2xl font-bold">
               {checkpoints.length > 0
-                ? formatRelativeTime(Math.max(...checkpoints.map((c) => c.submittedAt)))
+                ? formatBlockNumber(Math.max(...checkpoints.map((c) => c.blockNumber)))
                 : 'None'}
             </div>
           </CardContent>
@@ -146,7 +146,7 @@ function CheckpointsContent() {
                         </div>
                       </TableCell>
                       <TableCell className="text-gray-400">
-                        {formatRelativeTime(checkpoint.submittedAt)}
+                        {formatBlockNumber(checkpoint.blockNumber)}
                       </TableCell>
                     </TableRow>
                   ))}
