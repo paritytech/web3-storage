0ce94485965f55040f889b0a9355f1382ff31d8d
Daniel Bui <79790753+danielbui12@users.noreply.github.com>
2026-07-02 14:36:44 +0700

    chore: ignore weight files

--- runtimes/web3-storage-local/src/weights/pallet_drive_registry.rs
+++ runtimes/web3-storage-local/src/weights/pallet_drive_registry.rs
@@ -1,6 +1,6 @@
 // Copyright (C) Parity Technologies and the various Polkadot contributors, see Contributions.md
 // for a list of specific contributors.
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- runtimes/web3-storage-local/src/weights/xcm/mod.rs
+++ runtimes/web3-storage-local/src/weights/xcm/mod.rs
@@ -1,5 +1,5 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
